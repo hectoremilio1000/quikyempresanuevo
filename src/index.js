010027d7f9d14e03e240841cd4dcd1cfe2485ff5
hectoremilio1000 <hectoremilio1000@gmail.com>
2022-05-12 14:51:30 -0500

    layout

--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ import reportWebVitals from "./reportWebVitals";
 
 import Routing from "./Routes";
 
-import { Amplify } from "aws-amplify";
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
