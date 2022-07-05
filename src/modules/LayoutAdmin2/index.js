83a80ad61b6026242f49040d5cee06b0ef470c8f
hectoremilio1000 <hectoremilio1000@gmail.com>
2022-07-05 01:55:04 -0500

    cambio icono

--- src/modules/LayoutAdmin2/index.js
+++ src/modules/LayoutAdmin2/index.js
@@ -4,16 +4,16 @@ import { Button } from "react-bootstrap";
 import itemsRoutes2 from "./itemRoutes2";
 
 import HeaderLayoutAdmin2 from "./ContentLayout2/HeaderLayoutAdmin2";
-import CrearResultado from './ContentLayout2/CrearResultado';
+import CrearResultado from "./ContentLayout2/CrearResultado";
 
 import { Content } from "antd/lib/layout/layout";
-import ListaResultados from '../Components/Resultados/ListaResultados/index';
-import CrearResultado2 from './ContentLayout2/CrearResultado2/index';
+import ListaResultados from "../Components/Resultados/ListaResultados/index";
+import CrearResultado2 from "./ContentLayout2/CrearResultado2/index";
 
 const { Sider } = Layout;
 
 function LayoutAdmin2({ signOut, user, sub }) {
-    const [collapsed, setCollapsed] = useState(false);
+  const [collapsed, setCollapsed] = useState(false);
   const [current, setCurrent] = useState("");
 
   const toggle = () => {
@@ -22,13 +22,13 @@ function LayoutAdmin2({ signOut, user, sub }) {
   const cambiarComponent = e => {
     setCurrent(e.key);
   };
-  
+
   return (
     <>
-    <Layout>
+      <Layout>
         <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo" />
-        <Menu
+          <div className="logo" />
+          <Menu
             theme="dark"
             mode="inline"
             selectedKeys={current}
@@ -36,28 +36,35 @@ function LayoutAdmin2({ signOut, user, sub }) {
             items={itemsRoutes2}
           ></Menu>
         </Sider>
-    
-    <Layout>
-    <HeaderLayoutAdmin2 collapsed={collapsed} toggle={toggle} />
-    <Content style={{ margin: "24px 16px 0" }}>
-      {current === "1" ? (
-        <div className="site-layout-background" style={{ minHeight: 100 }}>
-          <CrearResultado/>
-        </div>
-      ) : current === "2" ? (
-        <div className="site-layout-background" style={{ minHeight: 100 }}>
-          <ListaResultados />
-        </div>
-      ) : <div></div>}
-      </Content>
-    </Layout>
-    </Layout>
-    <Button onClick={signOut} variant="warning" style={{ width: "100%" }}>
+
+        <Layout>
+          <HeaderLayoutAdmin2 collapsed={collapsed} toggle={toggle} />
+          <Content style={{ margin: "24px 16px 0" }}>
+            {current === "1" ? (
+              <div
+                className="site-layout-background"
+                style={{ minHeight: 100 }}
+              >
+                <CrearResultado />
+              </div>
+            ) : current === "2" ? (
+              <div
+                className="site-layout-background"
+                style={{ minHeight: 100 }}
+              >
+                <ListaResultados />
+              </div>
+            ) : (
+              <div></div>
+            )}
+          </Content>
+        </Layout>
+      </Layout>
+      <Button onClick={signOut} variant="warning" style={{ width: "100%" }}>
         Cerrar Sesión
       </Button>
-
     </>
-  )
+  );
 }
 
-export default LayoutAdmin2
+export default LayoutAdmin2;
