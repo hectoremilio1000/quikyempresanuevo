dc604790ef21f6953095a596325ec839dab9433a
hectoremilio1000 <hectoremilio1000@gmail.com>
2022-11-27 23:36:28 -0600

    reinicio

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 import Routing from "./Routes";
-import { Amplify } from "aws-amplify";
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Routing />);
